600a4331bcc6a4d814c2d4a04d067b16713fabf0
Timelaying <makanjuola.timi@gmail.com>
2025-01-01 20:59:34 +0000

    express works

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
 // imports 
-const express =  require('express') 
+const express =  require('express') // express is needed as a middle man for the API
+const app = express() // app eqauls to express
+
+app.listen(3000) // localhost port process to list to for request
+
