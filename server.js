cbe603f85b7b0a11d6a233222e4b3b0da0abd09f
Timelaying <makanjuola.timi@gmail.com>
2025-01-01 21:37:01 +0000

    jwt import

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express =  require('express') // express is needed as a middle man for the API
 const app = express() // app equals to express
 
+const jwt = require('jsonwebtoken')
+app.use(express.json)
+
 const posts = [ // dummy post fro testing
     {
         username: 'Kyle',
@@ -18,6 +21,10 @@ app.get('/posts', (req, res) => {
     res.json(posts) // reponds with the information of posts when the "/post" is called in url
 
 }) //get request to read information
+
+app.get('/login', (req, res) => { //Authenticate user
+     
+})
 
 app.listen(3000) // localhost port process to list to for request
 
