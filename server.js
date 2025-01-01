9011eebf5ec1777e24dd4e37a82e96db1d86000f
Timelaying <makanjuola.timi@gmail.com>
2025-01-01 21:05:10 +0000

    posts dummies

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 // imports 
 const express =  require('express') // express is needed as a middle man for the API
-const app = express() // app eqauls to express
+const app = express() // app equals to express
+
+const posts = [ // dummy post fro testing
+    {
+        username: 'Kyle',
+        title: 'Post 1'
+    }
+]
+
+app.get('/posts', (res, req) => {
+
+}) //get request to read information
 
 app.listen(3000) // localhost port process to list to for request
 
