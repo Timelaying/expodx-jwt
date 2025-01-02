3354217290e3b69708354907051138d7e9e59a20
Timelaying <makanjuola.timi@gmail.com>
2025-01-02 11:14:25 +0000

    filter post and middle ware auth

--- server.js
+++ server.js
@@ -20,18 +20,31 @@ const posts = [ // dummy post fro testing
     }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts) // reponds with the information of posts when the "/post" is called in url
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(post.filter(post => post.username === req.user.name)) //filtering post only by username inputed
+    //res.json(posts) // reponds with the information of posts when the "/post" is called in url
 }) //get request to read information
 
 app.post('/login', (req, res) => { //Authenticate user(this is done separatly, encryting the credentials basicaally)
      
     const username = req.body.username
     const user = { name: username }
-
+    //serializing user object
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET) //signing the token with env variables
     res.json({ accessToken: accessToken})
 })
+
+function authenticateToken(req, res, next){ // this is a middleware, should acctually be in another script for modularizarion
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return  req.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000) // localhost port process to list to for request
 
